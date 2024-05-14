23cb8b65e91883c81c91b1dfc785d42c5685a7f5
Anindo Roy <anindoroy112@gmail.com>
2024-05-14 22:33:32 +0600

    Navbar-update

--- src/app/Navbar/navbar.jsx
+++ src/app/Navbar/navbar.jsx
@@ -1,45 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import logo from "../../assets/shopnoshop.png";
 import "./navbar.css";
 import cartIcon from "../../assets/cart_icon.png";
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav className="navbar-header">
-      <div className="row">
-        <div className="nav-logo col ">
-          <Link to={"/Home"}>
-            <img src={logo} alt="" style={{ height: "80px" }} />
-          </Link>
-        </div>
+    <nav className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+      </div>
 
-        <div className="navbar-menu col text-decoration-none m-3">
-          <Link className="m-3 text-decoration-none" to={"/Home"}>
-            Home
-          </Link>
-          <Link className="m-3 text-decoration-none" to={"/Women"}>
-            Women
-          </Link>
-          <Link className="m-3 text-decoration-none" to={"/Men"}>
-            Men
-          </Link>
-          <Link className="m-3 text-decoration-none" to={"/Kids"}>
-            Kids
-          </Link>
-          <Link className="m-3 text-decoration-none" to={"/AboutUs"}>
-            About Us
-          </Link>
-        </div>
-        <div className="col gap-3 navbar-right-bar">
-          <div className="cartIcon">
-            <img src={cartIcon} alt="" />
-
-            <Link className="btn  gap-3 m-3">Login</Link>
-          </div>
-        </div>
+      <ul className="nav-menu">
+        <li>
+          Shop <hr />
+        </li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+        <li>About Us</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cartIcon} alt="" />
+        <div className="nav-cart-count">0</div>
       </div>
     </nav>
   );
-}
+};
 export default Navbar;
