9052b5592a86c32c33ddd588bf2e1782f23b9345
Anindo Roy <anindoroy112@gmail.com>
2024-05-18 23:56:43 +0600

    file_update

--- src/app/Navbar/navbar.jsx
+++ src/app/Navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../../assets/shopnoshop.png";
 import "./navbar.css";
 import cartIcon from "../../assets/cart_icon.png";
@@ -14,12 +14,50 @@ const Navbar = () => {
 
       <ul className="nav-menu">
         <li>
-          Shop <hr />
+          <Link
+            onClick={() => {
+              setMenu("shop");
+            }}
+          >
+            Shop {menu === "shop" ? <hr /> : <></>}
+          </Link>
         </li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
-        <li>About Us</li>
+        <li>
+          <Link
+            onClick={() => {
+              setMenu("men");
+            }}
+          >
+            Men{menu === "men" ? <hr /> : <></>}
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={() => {
+              setMenu("women");
+            }}
+          >
+            Women{menu === "women" ? <hr /> : <></>}
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={() => {
+              setMenu("kids");
+            }}
+          >
+            Kids{menu === "kids" ? <hr /> : <></>}
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={() => {
+              setMenu("about");
+            }}
+          >
+            About Us{menu === "about" ? <hr /> : <></>}
+          </Link>
+        </li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
